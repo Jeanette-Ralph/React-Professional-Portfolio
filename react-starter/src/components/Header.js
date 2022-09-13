97f47686072d64b09dc9aca898c89d3b244a4490
Jeanette Ralph <jean.ralph@hotmail.com>
2022-09-13 16:33:04 -0700

    added header component

---
+++ react-starter/src/components/Header.js
@@ -0,0 +1,16 @@
+import '../styles/Header.css'
+import '../styles/index.css'
+
+
+function Header() {
+    return(
+        <div className="header">
+            <header>
+                <h1>Hello! My name is Jeanette Ralph.</h1>
+                <h2>Full Stack Web Developer</h2>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
