385a530b1e1be1e11192d117a3ae2a763f500742
Jeanette Ralph <jean.ralph@hotmail.com>
2022-09-20 16:46:40 -0700

    pushing build

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Project from './components/Project';
@@ -18,6 +18,9 @@ function App() {
       <div>
       <Project />
       </div>
+    </div>
+    <div className='footer-div'>
+    <Footer />
     </div>
     </div>
   );
---
+++ react-starter/src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <h1 className="footer-name">Jeanette Ralph</h1>
+                <div className="footer-contact">
+                <p><a href="https://github.com/Jeanette-Ralph">Jeanette Ralph's GitHub</a></p>
+                <p><a href="mailto:jeanette.k.ralph@gmail.com">jeanette.k.ralph@gmail.com</a></p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
