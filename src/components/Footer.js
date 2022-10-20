ec600d66cad0e8298bdf915dcfd6f16f17c0398d
Jeanette Ralph <jean.ralph@hotmail.com>
2022-10-19 19:35:40 -0700

    added icons to contact section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,17 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-contact">
-        <p>
-          <a href="https://github.com/Jeanette-Ralph">
-            Jeanette Ralph's GitHub
-          </a>
-        </p>
-        <p>
-          <a href="https://www.linkedin.com/in/jeanette-ralph/">
-            Jeanette Ralph's LinkedIn
-          </a>
-        </p>
-        <p>
-          <a href="mailto:jeanette.k.ralph@gmail.com">
-            jeanette.k.ralph@gmail.com
-          </a>
-        </p>
+        <a href="https://www.linkedin.com/in/jeanette-ralph/">
+          <i className="fab fa-linkedin" />
+        </a>
+
+        <a href="mailto:jeanette.k.ralph@gmail.com">
+          <i className="fa fa-envelope"></i>
+        </a>
+
+        <a href="https://github.com/Jeanette-Ralph">
+          <i class="fa-brands fa-github"></i>
+        </a>
       </div>
     </footer>
   );
